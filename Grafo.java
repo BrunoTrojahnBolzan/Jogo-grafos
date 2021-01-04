abcced73b64fcc5960992ec6dddd45b442732bce
gabrielclopes <63819130+gabrielclopes@users.noreply.github.com>
2021-01-04 08:57:04 -0300

    Update Grafo.java

--- Grafo.java
+++ Grafo.java
@@ -1,54 +1,45 @@
 package com.gLstudios.IA;
 
-import java.util.*;
+import com.gLstudios.world.World;
 
 public class Grafo {
 
-	List<Nodo> g[];
+	int g[][] = new int[World.WIDTH][World.HEIGHT];
+	public int xInicio, yInicio;
+	
 	
 	class Nodo {
-		int vertice,weight;
+		int x, y, weight;
 		
-		public Nodo(int v,int w){
-		 	this.vertice=v;
+		public Nodo(int x, int y, int w){
+		 	this.x=x;
+		 	this.y=y;
 		 	this.weight = w;
-		}
-		public void teste() {
-			System.out.println("Teste");
 		}
 	} 
 	
-	public void addNodo(int u, int v, int w) {
-		g[u].add(u,new Nodo(v,w));
-	}
-	public void teste2() {
-		if(g[0].get(0).weight == 1)
-		{
-			System.out.println("okok!");
-		}
-		else
-			System.out.println("NãOnÂO");
-	}
-	
-	public Grafo(int n) {
-		g = new LinkedList[n];
-		for (int i=0; i<g.length; i++)
-		{
-			g[i] = new LinkedList<Nodo>();
-		} 
-		new Grafo.Nodo(10,10).teste();
-	}
-	
-	public void constroiGrafo() {
-		// apontamentos do 0
-		this.addNodo(0, 1, 0);
+	public void setWeight(int x, int y, int w) {
 		
-		this.addNodo(1, 2, 0);
-		this.addNodo(2, 3, 0);
-		this.addNodo(3, 4, 0);
-		this.addNodo(4, 5, 0);
+		g[x][y] = w;
+	}
+	
+	public void djikstra(int grafo[][], int xIni, int yIni) {
+		
 	}
 	
 	
+//	public Grafo() {
+//	}
+	
+	public void printgrafo() {
+		
+		for (int i=0; i<World.WIDTH; i++)
+		{
+			for (int j=0; j<World.HEIGHT; j++)
+				System.out.print(g[i][j]+" ");
+			System.out.println();
+		}
+			
+	}
 }
 

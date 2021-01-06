6f85ef6149c1863dd103045cb470255c9f0b44ae
gabrielclopes <63819130+gabrielclopes@users.noreply.github.com>
2021-01-06 16:25:05 -0300

    Update IA.java

--- IA.java
+++ IA.java
@@ -2,7 +2,12 @@ package com.gLstudios.IA;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
 
+import javax.swing.JOptionPane;
+
+import com.gLstudios.IA.Grafo.Coord;
 import com.gLstudios.entities.Entity;
 import com.gLstudios.main.Game;
 import com.gLstudios.world.Camera;
@@ -19,9 +24,35 @@ public class IA extends Entity{
 		Camera.x = Camera.clamp(getX() - (Game.WIDTH/2), 0, (World.WIDTH*16) - Game.WIDTH)  ;
 		Camera.y = Camera.clamp(getY() - (Game.HEIGHT/2), 0, (World.HEIGHT*16) - Game.HEIGHT);
 		
-		World.grafo.controlaRobo(World.grafo.caminho1);
+		controlaRobo(World.grafo.caminhoMenor);
 	}
 
+	
+	
+	public void controlaRobo(LinkedList<Coord> caminho1) {
+
+		try {
+			TimeUnit.MILLISECONDS.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	
+		if (caminho1.size() > 0)
+		{
+			Game.robo.setX(caminho1.getFirst().x * World.TILE_SIZE);
+			Game.robo.setY(caminho1.getFirst().y * World.TILE_SIZE);			
+			caminho1.removeFirst();
+		}
+		else
+		{
+			caminho1.clear();
+			JOptionPane.showMessageDialog(Game.frame, "Distância percorrida: " + World.grafo.menor);
+			System.exit(0);
+			Game.jogo.stop();
+		}
+	}
+	
+	
 	
 	public void render(Graphics g) {
 		g.drawImage(robo, this.getX() - Camera.x, this.getY() - Camera.y, null);	

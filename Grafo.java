e77c8542236ec6d2f7fb2587615d40145debcd84
gabrielclopes <63819130+gabrielclopes@users.noreply.github.com>
2021-01-06 12:38:27 -0300

    Update Grafo.java

--- Grafo.java
+++ Grafo.java
@@ -1,19 +1,24 @@
 package com.gLstudios.IA;
 
-import com.gLstudios.world.World;
-
+import java.awt.Component;
 import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
 
-import java.io.IOException;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import com.gLstudios.main.Game;
+import com.gLstudios.world.World;
 
 public class Grafo {
 
 	Nodo g[][] = new Nodo[World.WIDTH][World.HEIGHT];
+
 	public int xInicio, yInicio;
 	public int xFim, yFim;
 	int menor = 99999999;
 	
-	LinkedList<Coord> caminho1 = new LinkedList<Coord>();
+	public LinkedList<Coord> caminho1 = new LinkedList<Coord>();
 	LinkedList<Coord> caminho2 = new LinkedList<Coord>();
 	
 	class Nodo {
@@ -45,19 +50,12 @@ public class Grafo {
 	
 	public void dijkstra(int x, int y, int distancia) {
 		
-		try {
-			System.in.read();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		caminho2.add(new Coord(x, y));
+
 		if(!(x == xFim && y == yFim)) {
 			
 			g[x][y].visit = 1;
-			
-			caminho2.add(new Coord(x, y));
-			
+						
 			System.out.println("Visitado: " + x + "," + y);
 			
 			if(g[x][y - 1].peso != -1 && g[x][y - 1].visit == 0) {
@@ -72,15 +70,27 @@ public class Grafo {
 		
 		}else {
 			System.out.println("Distancia: " + distancia);
+			System.out.println(x + "  " + y);
+
 			
 			if(distancia < menor) {
 				menor = distancia;
 				caminho1 = (LinkedList<Coord>) caminho2.clone();
+//				for(int i=0;i<caminho2.size();i++)
+//					System.out.println(caminho2.get(i).x + "|" + caminho2.get(i).y);
 			}
 			
 		}
 		
-		caminho2.pop();
+		// correções para desempilhar
+		if(caminho2.size() > 1)
+			caminho2.removeLast() ;
+		else
+		{
+			caminho2.clear();
+			
+		}
+			
 		g[x][y].visit = 0;
 		
 	}
@@ -91,6 +101,34 @@ public class Grafo {
 			for (int j=0; j<World.HEIGHT; j++) {
 				g[i][j] = new Nodo(0);
 			}
+
+		}
+	}
+	
+	public void controlaRobo(LinkedList<Coord> caminho) {
+		try {
+			TimeUnit.MILLISECONDS.sleep(300);;
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+//		System.out.println(caminho1.get(0));
+//		
+	
+		if (caminho1.size() > 0)
+		{
+			System.out.println("X: " + this.caminho1.getFirst().x + "Y: "+ this.caminho1.getFirst().y);
+			Game.robo.setX(this.caminho1.getFirst().x * World.TILE_SIZE);
+			Game.robo.setY(this.caminho1.getFirst().y * World.TILE_SIZE);			
+			caminho1.removeFirst() ;
+
+		}
+		else
+		{
+			caminho1.clear();
+			JOptionPane.showMessageDialog(Game.frame, "Distância percorrida: " + menor);
+			System.exit(0);
+
+			Game.jogo.stop();
 		}
 	}
 	
